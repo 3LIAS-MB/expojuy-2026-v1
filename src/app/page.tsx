7baa0a975f80a0fbbbdbd61a176e74b85aba7da1
EliDev <eliasss.mb@gmail.com>
2026-09-08 22:53:45 -0300

    feat: actualizar SponsorsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { ExhibitorsSection } from "@/components/prototype/exhibitors-section";
 import { HeroSection } from "@/components/prototype/hero-section";
 import { SiteHeader } from "@/components/prototype/site-header";
 
-import SponsorsSection from "@/components/SponsorsSection";
 import FaqSection from "@/components/FaqSection";
 import { Footer } from "@/components/layout/Footer";
 
@@ -47,7 +46,6 @@ export default function Home() {
         <HeroSection introFinished={introFinished} />
         <AboutExperience />
         <ExhibitorsSection />
-        <SponsorsSection />
         <FaqSection />
       </main>
 
